31275bf1173da9492ebfd2b86130a5e759d4b2c2
bba8557 <bba8557@naver.com>
2022-05-12 15:13:29 +0900

    22.05.12

--- Ex02.sql
+++ Ex02.sql
@@ -11,6 +11,8 @@ from employees;
 select *
 from departments;
 
+-------------------------------------------------------------------------------
+
 --원하는 컬럼만 조회하기
 select employees_id, 
        first_name, 
@@ -54,6 +56,8 @@ select first_name as 이름, -- as="" 같은 역할(생략도 가능)
        hire_date
 from employees;
 
+-------------------------------------------------------------------------------
+
 --연결연산자(커럼을 붙이기)
 select first_name, --이름
        last_name   --성
@@ -65,6 +69,8 @@ from employees;
 
 select first_name || ' hire date is ' || hire_date 입사일
 from employees;
+
+-------------------------------------------------------------------------------
 
 --산술 연산자
 select first_name, 
@@ -89,6 +95,8 @@ select first_name || ' _ ' || last_name 성명,
        phone_number 전화번호
 from employees;
 
+-------------------------------------------------------------------------------
+
 /*where절 (조건을 붙일수있다?)*/
 select first_name
 from employees
@@ -111,6 +119,8 @@ where hire_date >= '07/01/01';
 select salary 연봉
 from employees
 where first_name >= 'Lex';
+
+-------------------------------------------------------------------------------
 
 --조건이 2개이상 일때 한꺼번에 조회하기 (and연상자 - 둘다 or연상자 - 하나만)
 --연봉이 14000이상 17000이하인 사원의 이름과 연봉을 출력하세요
@@ -139,6 +149,8 @@ select first_name,
        salary
 from employees
 where salary BETWEEN 14000 and 17000;
+
+-------------------------------------------------------------------------------
 
 --IN 연산자로 여러 조건을 검사하기
 select first_name,
@@ -169,3 +181,84 @@ where salary = 2100
     or salary = 3100
     or salary = 4100
     or salary = 5100;
+    
+-------------------------------------------------------------------------------
+
+--Like 연산자로 비슷한것들 모두 찾기  % = 뭐가있든 상관없다 _ = 자리수를 나타낼수있다
+select first_name, last_name, salary
+from employees
+where first_name like 'L%'; --뒤에는 상관없고 앞이 L인것
+
+select first_name, last_name, salary
+from employees
+where first_name like '%s'; --앞은 상관없고 맨뒤에 s인것
+
+select first_name, last_name, salary
+from employees
+where first_name like '%s%'; --s있는건 다
+
+select first_name, last_name, salary
+from employees
+where first_name like 'L___'; --몇자
+
+--예제)
+--이름에 am 을 포함한 사원의 이름과 연봉을 출력하세요
+select first_name, 
+       salary
+from employees
+where first_name like '%am%'; 
+
+--이름의 두번째 글자가 a인 사원의 이름과 연봉을 출력하세요
+select first_name, 
+       salary
+from employees
+where first_name like '_a%';
+
+--이름의 네번째 글자가 a인 사원의 이름을 출력하세요
+select first_name
+from employees
+where first_name like '___a%';
+
+--이름이 4글자인 사원중 끝에서 두번째 글자가 a인 사원의 이름을 출력하세요
+select first_name
+from employees
+where first_name like '__a_';
+
+-------------------------------------------------------------------------------
+
+select first_name,
+       salary,
+       commission_pct,
+       salary*commission_pct 
+from employees
+where salary between 13000 and 15000;
+
+--수당 없는사람 null
+select first_name,
+       salary,
+       commission_pct,
+       salary*commission_pct 
+from employees
+where commission_pct is null;
+
+--수당 있는사람 
+select first_name,
+       salary,
+       commission_pct,
+       salary*commission_pct 
+from employees
+where commission_pct is not null;
+
+--예제)
+--커미션비율이 있는 사원의 이름과 연봉 커미션비율을 출력하세요
+select first_name,
+       salary,
+       commission_pct
+from employees
+where commission_pct is not null;
+
+--담당매니저가 없고 커미션비율이 없는 직원의 이름을 출력하세요
+select first_name
+from employees
+where manager_id is null 
+    and commission_pct is null;
