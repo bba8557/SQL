3337b8740777ca13f180afb724465bfcc67d765a
bba8557 <bba8557@naver.com>
2022-05-18 16:36:38 +0900

    서브쿼리+단일행+다중행

--- Ex/Ex05.sql
+++ Ex/Ex05.sql
@@ -1,85 +1,68 @@
---rownum
+/***********************
+        SubQuery 
+************************/
 
---급여를 가장 많이 받는 5명의 직원의 이름을 출력하시오.
---급여순으로 정렬
-select employee_id,
-        first_name,
-        salary
+--subquery(단일행) 
+
+--'Den' 보다 급여를 많은 사람의 이름과 급여는?
+select first_name,
+       salary
 from employees
+where first_name = 'Den'; --11000
+
+select first_name,
+       salary
+from employees
+where salary >= (select salary
+                 from employees
+                 where first_name = 'Den')
 order by salary desc;
 
---1~5등만 출력
---rownum 가 먼저 적용 desc는 나중에 적용 되기에 순서가 깨진다
-select rownum, 
-       employee_id,
+--급여를 가장 적게 받는 사람의 이름, 급여, 사원번호는?
+select first_name,
+       salary,
+       employee_id
+from employees
+--order by salary asc; --TJ 2100 50
+where salary = (select min(salary)
+                from employees);
+
+--가장 작은 급여를 구한다
+select min(salary)
+from employees;
+
+--평균 급여보다 적게 받는 사람의 이름, 급여를 출력하세요
+--(1)평균 급여를 구한다
+select avg(salary)
+from employees;
+
+--(2)
+select first_name,
+       salary
+from employees
+where salary <= (select avg(salary)
+                 from employees)
+order by salary asc;
+
+----------------------------------------------------------------------------------
+--subquery(다중행) 
+
+--?? 부서번호가 110인 직원의 급여와 같은 모든 직원의 사번, 이름, 급여를 출력하세요
+--110 12008
+--110 8300
+
+select salary
+from employees
+where department_id = 110;
+
+select employee_id,
        first_name,
        salary
 from employees
-order by salary desc;
-
-select rownum, --번호가 부여되도 깨지지않는다
-       ot.employee_id,
-       ot.first_name,
-       ot.salary,
-       ot.phone_number --추가시 사용 가능
-       --ot.phone_number 테이블에 추가가 되지 않았기에 쓸수 없다
-from (select employee_id, --하나로 묶어줘서 정렬
-       first_name,
-       salary,
-       phone_number
-       from employees
-       order by salary desc) ot
-where rownum >= 1
-and rownum <= 5;
-
---꼭 1번이 아니라도 출력 가능하게 
-select ort.rn,
-       ort.employee_id,
-       ort.first_name,
-       ort.salary
-from (select rownum rn,
-             ot.employee_id,
-             ot.first_name,
-             ot.salary
-      from (select employee_id,
-                   first_name,
-                   salary
-            from employees
-            order by salary desc) ot 
-      ) ort
-where rn >= 1
-and rn <= 5;
-
---예제) 07년에 입사한 직원중 급여가 많은 직원중 3에서 7등의 이름 급여 입사일은?
---07년 입사한 직원들 급여 많은순
-select first_name,
-       salary,
-       hire_date
-from employees
-where hire_date >= '07/01/01'
-and hire_date <= '07/12/31'
-order by salary desc;
-
---3~7등
-select hsr.rn,
-       hsr.employee_id,
-       hsr.first_name,
-       hsr.salary,
-       hsr.hire_date
-from (select rownum rn,
-             hs.employee_id,
-             hs.first_name,
-             hs.salary,
-             hs.hire_date
-      from (select employee_id,
-                   first_name,
-                   salary,
-                   hire_date
-            from employees
-            where hire_date >= '07/01/01'
-            and hire_date <= '07/12/31'
-            order by salary desc) hs 
-      ) hsr
-where hsr.rn >= 3
-and hsr.rn <= 7;
-
+--비교
+/*where salary = 12008
+or salary = 8300;*/
+--사용
+where salary in (select salary
+                 from employees
+                 where department_id = 110);  --12008, 8300
