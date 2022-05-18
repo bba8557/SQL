971f9411be662e74eeaaf3e8f6fde936d117a25b
bba8557 <bba8557@naver.com>
2022-05-18 16:37:39 +0900

    그룹함수

--- Ex/Ex03.sql
+++ Ex/Ex03.sql
@@ -1,121 +1,141 @@
 /**********************
-* Join 
+* 그룹함수
 ***********************/
---equi join
-select *
-from employees, departments;
+/*
+select  first_name,
+         sum(salary)
+from employees;
+*/
+select sum(salary)
+from employees;
 
-select  employee_id ,
+--그룹함수 - count()
+select  *
+from employees;
+
+select  count(*),
+        count(commission_pct),  --commission_pct 컬럼의 null 카운트에서 제외
+        count(manager_id)
+from employees;
+
+select count(*)
+from employees
+where salary > 16000;
+
+--그룹함수 - sum()
+select  count(salary),
+        sum(salary)
+from employees;
+
+--그룹함수 - avg()
+select  avg(salary),        --급여가 null인 사람은 평균계산 제외 
+        avg(nvl(salary, 0)), --null인값은 0으로 변경후 평균계산
+        round(avg(salary), 0),  -- 반올림
+        count(*),
+        sum(salary)
+from employees;
+
+--그룹함수 - max() / min()
+select  max(salary),
+        min(salary),
+        count(*)
+from employees;
+
+--GROUP BY 절
+--절 구성
+select first_name
+from employees
+where salary > 16000
+order by salary desc;
+
+/*
+select  department_id,  -- 여러개
+        avg(salary) --전체평균 1개
+from employees;
+*/
+
+select  department_id,
+        job_id,
+        avg(salary),  -- department_id(부서별 평균)
+        sum(salary),  -- department_id(부서별 합계)
+        count(salary) -- department_id(부서별 카운트)
+from employees
+group by department_id, job_id
+order by department_id asc;
+
+--연봉(salary)의 합계가 20000 이상인 부서의 
+--부서 번호와 , 인원수, 급여합계를 출력하세요
+select  department_id,
+        count(*),
+        sum(salary)
+from employees
+where sum(salary)>= 20000   --where절에는 그룹함수를 쓸수 없다
+group by department_id;
+
+--having 절
+--정상
+select  department_id,
+        count(*),
+        sum(salary)
+from employees
+group by department_id
+having sum(salary) >= 20000
+and sum(salary) <= 100000
+and department_id = 90;
+
+--오류
+select  department_id,
+        count(*),
+        sum(salary)
+from employees
+group by department_id
+having sum(salary) >= 20000
+and sum(salary) <= 100000
+and department_id = 90
+and hire_date >= '04/01/01';   /*having 절에는 그룹함수 와 
+                                 Group by에 참여한 컬럼만 사용할 수 있다.
+                                */
+
+
+--CASE ~ END 문
+select  employee_id,
         first_name,
+        job_id,
         salary,
-        department_name,
-        em.department_id "e_did",
-        de.department_id "d_did"
-from employees em, departments de
-where em.department_id = de.department_id;
+        case when job_id = 'AC_ACCOUNT' then salary+salary*0.1
+             when job_id = 'SA_REP' then salary+salary*0.2
+             when job_id = 'ST_CLERK' then salary+salary*0.3
+             else salary
+        END realSalary
 
---모든 직원이름, 부서이름, 업무명 을 출력하세요
-select  e.first_name,
-        d.department_name,
-        j.job_title,
-        e.salary,
-        e.department_id,
-        d.department_id,
-        e.job_id,
-        j.job_id
-from employees e, departments d, jobs j
-where e.department_id = d.department_id
-and e.job_id = j.job_id
-and e.salary >= 7000
-order by salary desc
-;
-
---left join
-
---equi join 테이터 106개 --> null은 포함되지 않는다
-select  e.first_name,
-        e.department_id,
-        d.department_name,
-        d.department_id
-from employees e, departments d
-where e.department_id = d.department_id;
-
---left join 예제
-select  e.first_name,
-        e.department_id,
-        d.department_name,
-        d.department_id
-from employees e left outer join departments d
-on e.department_id = d.department_id;
-
---left join 오라클 표현법
-select  e.first_name,
-        e.department_id,
-        d.department_name,
-        d.department_id
-from employees e, departments d
-where e.department_id = d.department_id(+);
+from employees;
 
 
---right join 
-
---equi join 테이터 106개 --> null은 포함되지 않는다
-select  e.first_name,
-        e.department_id,
-        d.department_name,
-        d.department_id
-from employees e, departments d
-where e.department_id = d.department_id;
-
---right join 예제
-select  e.first_name,
-        e.department_id,
-        d.department_name,
-        d.department_id
-from employees e right outer join departments d
-on e.department_id = d.department_id;
-
---right join 오라클 표현법
-select  e.first_name,
-        e.department_id,
-        d.department_name,
-        d.department_id
-from employees e, departments d
-where e.department_id(+) = d.department_id;
-
---right join --> left join
-select  e.first_name,
-        e.department_id,
-        d.department_name,
-        d.department_id
-from employees e right outer join departments d
-on e.department_id = d.department_id;
 
 
-select  e.first_name,
-        e.department_id,
-        d.department_name,
-        d.department_id
-from departments d left outer join employees e
-on e.department_id = d.department_id;
+--DECODE 문
+select  employee_id,
+        first_name,
+        job_id,
+        salary,
+        decode( job_id, 'AC_ACCOUNT', salary+salary*0.1, 
+                        'SA_REP', salary+salary*0.2,
+                        'ST_CLERK', salary+salary*0.3 ,  
+                salary ) bonus
+from employees;
 
---full outer join
-select  e.first_name,
-        e.department_id,
-        d.department_name,
-        d.department_id
-from employees e full outer join departments d
-on e.department_id = d.department_id;
-
-
---*Self Join
-select  e.employee_id,
-        e.first_name,
-        e.salary,
-        e.phone_number,
-        e.manager_id,
-        m.employee_id,
-        m.first_name ManagerName,
-        m.phone_number
-from employees e, employees m
-where e.manager_id = m.employee_id;
+--직원의 이름, 부서, 팀을 출력하세요
+--팀은 코드로 결정하며 
+--부서코드가 10~50 이면 ‘A-TEAM’
+--          60~100이면 ‘B-TEAM’  
+--          110~150이면 ‘C-TEAM’ 
+--          나머지는 ‘팀없음’ 
+--으로 출력하세요.
+select  first_name,
+        department_id,
+        case when department_id>=10 and department_id<=50 then 'A팀'
+             when department_id>=60 and department_id<=100 then 'B팀'
+             when department_id>=110 and department_id<=150 then 'C팀'
+             else '팀없음'
+        end team
+from employees;
